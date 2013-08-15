7fbff6dd8b9524764f3189d0cff650892376ed9e
Bernard Duchesne <bernard@kiluna.com>
2013-08-15 15:20:55 +1000

    Initial commit

---
+++ templates/user-profile.tpl.php
@@ -0,0 +1,4 @@
+<div class="iin-user-profile">
+  <div id="user-picture"><?php print render ($user_profile['user_picture']); ?></div>
+  <div id="kickstarter-profile"><?php print render ($user_profile['profile_iin_kickstarter']); ?></div>
+</div>
