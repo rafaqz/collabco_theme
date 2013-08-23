a78dee7aac11d8ea9337dbee1496b9ff2406a6d7
Rafael Schouten <rafaelschouten@gmail.com>
2013-08-23 11:06:07 +1000

    replace iin-kickstarter with profile

--- templates/user-profile.tpl.php
+++ templates/user-profile.tpl.php
@@ -1,4 +1,4 @@
 <div class="iin-user-profile">
   <div id="user-picture"><?php print render ($user_profile['user_picture']); ?></div>
-  <div id="kickstarter-profile"><?php print render ($user_profile['profile_iin_kickstarter']); ?></div>
+  <div id="profile"><?php print render ($user_profile['profile_profile']); ?></div>
 </div>
