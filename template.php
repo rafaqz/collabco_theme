588bb680dc65cf07710b798ca2188a6f55d8ab7b
Rafael Schouten <rafaelschouten@gmail.com>
2013-09-16 18:15:36 +1000

    theme knowledge feature file and entity ref fields

--- template.php
+++ template.php
@@ -6,13 +6,16 @@
 function collabco_theme_field_widget_form_alter(&$element, &$form_state, $context) {
   if (!empty($element['#field_name'])) {
     switch ($element['#field_name']) {
+      // Fixes for file and entity ref fields. These could possibly go in the 
+      // knowledge feature instead of here...
       case 'field_ko_file':
         unset($element['#theme_wrappers']);
         unset($element['#description']);
         $element['#file_upload_description'] = '';
         break;
-      case 'og_group_ref':
-        unset($element['#theme_wrappers']);
+      case 'field_ko_ref':
+        $help = t('Or select a file you have allready uploaded elsewhere');
+        $element['#field_prefix'] = $help;
         break;
     }
   }
